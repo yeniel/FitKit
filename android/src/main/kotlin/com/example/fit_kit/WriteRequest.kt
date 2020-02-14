12d1a830e9270695de2a5fe3d751165c02387f5f
Yeniel Landestoy <y3ni3l@gmail.com>
2020-02-14 11:11:06 +0100

    Android bugfixing

--- android/src/main/kotlin/com/example/fit_kit/WriteRequest.kt
+++ android/src/main/kotlin/com/example/fit_kit/WriteRequest.kt
@@ -2,6 +2,7 @@ package com.example.fit_kit
 
 import io.flutter.plugin.common.MethodCall
 import java.util.*
+import android.util.Log
 
 abstract class WriteRequest<T : Type> private constructor(
         val type: T,
@@ -20,6 +21,8 @@ abstract class WriteRequest<T : Type> private constructor(
     companion object {
         @Throws
         fun fromCall(call: MethodCall): WriteRequest<Type.Activity> {
+            Log.d("FitKit", "fromCall ${call.argument<String>("type")}")
+
             val type = call.argument<String>("type")?.fromDartType()
                     ?: throw Exception("type is not defined")
             val dateFrom = safeLong(call, "date_from")?.let { Date(it) }
@@ -28,6 +31,10 @@ abstract class WriteRequest<T : Type> private constructor(
                     ?: throw Exception("date_to is not defined")
             val name = call.argument<String>("name")
             val description = call.argument<String>("description")
+
+            if (type is Type.Sample) {
+                Log.d("FitKit", "fromCall type is Type.Sample")
+            }
 
             return when (type) {
                 is Type.Sample -> throw Exception("type is not defined")
